1ab50133b0c41dbb6bd5d5fa2466593cb86a8009
とーふとふ <tohu.soy@gmail.com>
2020-02-26 09:00:38 +0900

    初期設定

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: "Webエンジニアになろう",
+  description: "Webエンジニア初心者向け資料",
+  markdown: {
+    lineNumbers: true
+  },
+  toc: {
+    includeLevel: [1, 2]
+  },
+  evergreen: true
+};
